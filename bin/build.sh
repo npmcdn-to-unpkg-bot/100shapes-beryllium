57193f554465469c2664b00c9b22494a51af561b
Michele Memoli <michele@100shapes.com>
2016-03-21 22:24:55 +0000

    build(chore) More travis build updates

---
+++ bin/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./node_modules/.bin/hexo clean
+./node_modules/.bin/hexo generate
---
+++ bin/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS" = "true" ]
+then
+  # git need this, on Travis-CI nobody is defined
+  git config --global user.email "gh-pages@localhost"
+  git config --global user.name "npm gh-pages"
+
+  ./node_modules/.bin/gh-pages --dist public --repo https://$GITHUB_TOKEN@github.com/100Shapes/100shapes-beryllium.git
+  --silent
+  .
+else
+  ./node_modules/.bin/gh-pages --dist public
+fi
+
+

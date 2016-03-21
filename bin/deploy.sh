3a50cce67910c8e4f4d8ade20a447edf628eb701
Michele Memoli <michele@100shapes.com>
2016-03-21 22:54:44 +0000

    build(chore) travis

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,9 +6,7 @@ then
   git config --global user.email "gh-pages@localhost"
   git config --global user.name "npm gh-pages"
 
-  ./node_modules/.bin/gh-pages --dist public \
-  --repo https://$GH_TOKEN@github.com/100Shapes/100shapes-beryllium.git \
-  --silent
+  ./node_modules/.bin/gh-pages --dist public --repo https://$GH_TOKEN@github.com/100Shapes/100shapes-beryllium.git --silent
   .
 else
   ./node_modules/.bin/gh-pages --dist public
